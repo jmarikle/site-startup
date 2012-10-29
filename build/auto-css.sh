4043a313d23990bfa4c2a519fbafcd5e4420aa2a
Joseph Marikle <joe@44interactive.com>
2012-10-29 17:42:21 -0500

    added message for css compile

--- build/auto-css.sh
+++ build/auto-css.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Auto Compile LESS
-./monitor.py --dir ../less/ --action 'lessc ../less/bootstrap.less > ../css/style.css && cleancss -o ../css/style-min.css ../css/style.css'
+./monitor.py --dir ../less/ --action 'echo $(date +%H:%M:%S) compiling LESS...; lessc ../less/bootstrap.less > ../css/style.css && cleancss -o ../css/style-min.css ../css/style.css && cp ../css/style-min.css /Volumes/Source\ Code/sdagc.org/site/css/style-min.css;  echo $(date +%H:%M:%S) done'
