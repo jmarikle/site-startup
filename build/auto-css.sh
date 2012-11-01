85c9b9e1dd3742472f7015289b3c5a6ad038b4e2
Joseph Marikle <joe@44interactive.com>
2012-11-01 16:41:01 -0500

    auto output to dev environment added

--- build/auto-css.sh
+++ build/auto-css.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
 # Auto Compile LESS
-./monitor.py --dir ../less/ --action 'echo $(date +%H:%M:%S) compiling LESS...; lessc ../less/bootstrap.less > ../css/style.css && cleancss -o ../css/style-min.css ../css/style.css && cp ../css/style-min.css /Volumes/Source\ Code/sdagc.org/site/css/style-min.css;  echo $(date +%H:%M:%S) done'
+./monitor.py --dir ../less/ --action '
+    echo $(date +%H:%M:%S) compiling LESS...;
+    lessc ../less/bootstrap.less > ../css/style.css && cleancss -o ../css/style-min.css ../css/style.css;
+    echo $(date +%H:%M:%S) done compiling;
+    sourceDir=${PWD%/*/*};
+    sourceDir=${sourceDir##*/};
+    sourceDir="/Volumes/Source Code/"$sourceDir"/site/css";
+    if [ -d "$sourceDir" ]; then
+        echo $(date +%H:%M:%S) "copying compiled output to dev...";
+        cp ../css/style-min.css "$sourceDir";
+        echo $(date +%H:%M:%S) done copying;
+    fi
+    echo $(date +%H:%M:%S) done;
+    echo ------------------------------------------------------;
+'
